87eca4ea803a83bbeff05ed802c3124c7a3fd8c9
Liso Gallo <liso@riseup.net>
2017-07-27 13:12:30 -0300

    Initial commit

---
+++ remove_odoo_bindings/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Based on 'disable_odoo_online' module by Therp BV
+# Copyright (C) 2013 Therp BV (<http://therp.nl>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Remove odoo.com Bindings",
+    "version": "10.0.1.0.0",
+    "author": "Therp BV, GRAP, Odoo Community Association (OCA), TMC Rosario",
+    "license": "AGPL-3",
+    "category": "base",
+    "depends": [
+        'base'
+    ],
+    "data": [
+        'views/ir_ui_menu.xml'
+    ],
+    "qweb": [
+        'static/src/xml/base.xml',
+    ],
+    'installable': True,
+}
