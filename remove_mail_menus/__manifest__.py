6d5b29e77cde91c8ad340cce622aace2b864b3a1
Liso Gallo <liso@riseup.net>
2019-10-31 12:13:42 -0300

    [ADD] 'remove_mail_menus' addon to hide Activities, Conversation and Discuss menus

---
+++ remove_mail_menus/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Remove 'mail' addon menus",
+    "version": "13.0.1.0.0",
+    "author": "TMC Rosario",
+    "license": "AGPL-3",
+    "category": "base",
+    "data": [
+        'data/mail_channel_views.xml'
+    ],
+    "qweb": [
+        'static/src/xml/systray.xml',
+    ],
+    'installable': True,
+}  # yapf: disable
