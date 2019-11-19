6662c9fe5f2d8a39b2b61544d829a07fe43e8ff9
Liso Gallo <liso@riseup.net>
2019-11-19 13:25:49 -0300

    [ADD][IMP] Optional message in HTML

--- popup_message_dialog_box/models/popup_message.py
+++ popup_message_dialog_box/models/popup_message.py
@@ -4,9 +4,21 @@ from odoo import fields, models
 class PopupMessage(models.TransientModel):
     _name = 'popup.message'
 
-    def _default_name(self):
+    def _default_message(self):
         if self.env.context.get('message', False):
             return self.env.context.get('message')
         return False
 
-    name = fields.Text(string='Message', readonly=True, default=_default_name)
+    def _default_is_html(self):
+        self.env.context.get('is_html', False)
+        return self.env.context.get('is_html')
+
+    plain_text_message = fields.Text(string='Message',
+                                     readonly=True,
+                                     default=_default_message)
+
+    html_message = fields.Html(string='Message',
+                               readonly=True,
+                               default=_default_message)
+
+    is_html = fields.Boolean(readonly=True, default=_default_is_html)
