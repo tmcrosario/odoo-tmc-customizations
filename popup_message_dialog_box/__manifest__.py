3755b858bc6034c015b05ee82b6e76fadce255d9
Lisandro Gallo <4266083+lisandrogallo@users.noreply.github.com>
2022-07-20 13:48:46 -0300

    [MIG] Migration to 14.0

--- popup_message_dialog_box/__manifest__.py
+++ popup_message_dialog_box/__manifest__.py
@@ -1,13 +1,11 @@
 {
-    'name': 'Popup Message Dialog Box',
-    'version': '13.0.1.0.0',
-    'category': 'Extra Tools',
-    'author': 'Lisandro Gallo',
-    'license': 'AGPL-3',
-    'depends': [],
-    'data': [
-        'views/popup_message_views.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
+    "name": "Popup Message Dialog Box",
+    "version": "14.0.1.0.0",
+    "category": "Extra Tools",
+    "author": "Lisandro Gallo",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": ["views/popup_message_views.xml"],
+    "installable": True,
+    "auto_install": False,
 }  # yapf: disable
--- popup_message_dialog_box/models/popup_message.py
+++ popup_message_dialog_box/models/popup_message.py
@@ -3,24 +3,24 @@ from odoo import fields, models
 
 class PopupMessage(models.TransientModel):
 
-    _name = 'popup.message'
-    _description = 'Popup Message'
+    _name = "popup.message"
+    _description = "Popup Message"
 
     def _default_message(self):
-        if self.env.context.get('message', False):
-            return self.env.context.get('message')
+        if self.env.context.get("message", False):
+            return self.env.context.get("message")
         return False
 
     def _default_is_html(self):
-        self.env.context.get('is_html', False)
-        return self.env.context.get('is_html')
+        self.env.context.get("is_html", False)
+        return self.env.context.get("is_html")
 
-    plain_text_message = fields.Text(string='Message',
-                                     readonly=True,
-                                     default=_default_message)
+    plain_text_message = fields.Text(
+        string="Message", readonly=True, default=_default_message
+    )
 
-    html_message = fields.Html(string='Message',
-                               readonly=True,
-                               default=_default_message)
+    html_message = fields.Html(
+        string="Message", readonly=True, default=_default_message
+    )
 
     is_html = fields.Boolean(readonly=True, default=_default_is_html)
